742fb00fcbf3b5a07a2c4cdb2ce2b25936fe9205
shotosray9 <ssinhara@purdue.edu>
2019-07-18 23:41:08 -0700

    Changes to gradle and other set up stuff

--- app/src/main/java/com/example/shoto/gotothis/MainActivity.java
+++ app/src/main/java/com/example/shoto/gotothis/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
